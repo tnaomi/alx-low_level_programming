684bb05fdf203ce0c36038c9040d5d4ce12d7088
Tadala <naomikape@gmail.com>
2023-08-10 12:36:03 -0700

    Removed newline

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@ int main(void)
 {
 char str1[68];
 
-strcpy(str1, "\"Programming is like building a multilingual puzzle\n");
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
 
 puts(str1);
 return (0);
