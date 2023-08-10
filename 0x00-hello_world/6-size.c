93b628720a96f96593ca3b0214b9253cc4dce143
Tadala <naomikape@gmail.com>
2023-08-10 13:05:25 -0700

    Separated (s)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@ int main(void)
 	long long longlongNo;
 	float floatNo;
 
-	printf("Size of a char: %zu bytes\n", sizeof(character));
-	printf("Size of an int: %zu bytes\n", sizeof(intNo));
-	printf("Size of a long int: %zu bytes\n", sizeof(longNo));
-	printf("Size of a long long int: %zu bytes\n", sizeof(longlongNo));
-	printf("Size of a float: %zu bytes\n", sizeof(floatNo));
+	printf("Size of a char: %zu byte(s)\n", sizeof(character));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intNo));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longNo));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongNo));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatNo));
 
 	return (0);
 }
