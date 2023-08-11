f294142657bed3b573dba99d7561992972c8a0fa
Tadala <naomikape@gmail.com>
2023-08-11 14:05:16 -0700

    Used while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints a single digit number starting from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int single = 0;
+
+	for (single = 0;
+	single < 10;
+	single++)
+	{
+		printf("%d", single);
+	}
+	return (0);
+}
