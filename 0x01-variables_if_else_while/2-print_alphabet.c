00ff9f1ce4f07a450980225d00f1a83982402dbb
Tadala <naomikape@gmail.com>
2023-08-11 12:05:33 -0700

    Used for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char cha;
+
+	for (cha = 'a';
+	cha <= 'z';
+	cha++)
+	{
+		putchar(cha);
+	}
+	putchar('\n');
+}
