b179e6cbdd9f384d8be2b9bcaeb5e096f4159cc1
Tadala <naomikape@gmail.com>
2023-08-11 10:05:58 -0700

    Fixed Character Count

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,11 +27,11 @@ int main(void)
 	else
 		if (mod > 5)
 		{
-		printf("and %d is greater than 5\n", mod);
+		printf("and is greater than 5\n", mod);
 		}
 		else
 		{
-		printf("and %d is less than 6 and not 0\n", mod);
+		printf("and is less than 6 and not 0\n", mod);
 		}
 	return (0);
 
