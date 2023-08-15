547c8b44b4a7707e958734a73d91423ee9000611
Tadala <naomikape@gmail.com>
2023-08-15 13:46:57 -0700

    Used while loop

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_to_98- counts from n to 98
+ * @n: to count from n to 98
+ * Return: integers list
+ */
+
+void print_to_98(int n)
+{
+
+	int n;
+	n=0;
+
+	while (n < 99)
+	{
+		return (n);
+		_putchar(',');
+		n++;
+	}
+}
