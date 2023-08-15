87de80d9f307e455a5579e80fa564f6a88cc1b9b
Tadala <naomikape@gmail.com>
2023-08-15 07:09:49 -0700

    Used 97 Hex

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - checks whether 'c' is lowercase,
+ * returns '1'-lowercase, '0'-uppercase
+ */
+
+int _islower(int c)
+{
+	int a = 97;
+
+	if (c >= a)
+	{
+		_putchar('1');
+	}
+
+	else
+	{
+		_putchar('0');
+	}
+
+}
