5a48aa21104045110b4a25a84ce3368c0ae15e87
Tadala <naomikape@gmail.com>
2023-08-15 02:43:13 -0700

    Used arrays putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,25 @@
-#include <std.io>
+#include <stdio.h>
+
+/**
+* main - The function puts out 'putchar'
+*without usin built-in functions
+*other than putchar.
+* Return : Always 0 (Success)
+*/
 
 int main(void)
 {
+	int chars[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int index;
+
+	for (index = 0;
+	index <= 7;
+	index++)
+	{
+		putchar(chars[index]);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
