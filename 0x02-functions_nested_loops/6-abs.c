36854cb2abfc8885f893fe6ded6d3b519829a7c7
Tadala <naomikape@gmail.com>
2023-08-15 11:13:01 -0700

    Task 6 Initial

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - displays an absolute val of n,
+ * @n : the variable to be checked
+ * Return: an absolute positivd integer
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+
+	return (n)
+
+}
