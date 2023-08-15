e3f1adf8eba973e99f875da85bd80b823a1ea68b
Tadala <naomikape@gmail.com>
2023-08-15 06:21:32 -0700

    Used nested for_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the letters of the alphabet
+ *followed by a newline
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int times;
+
+	char alpha;
+
+	for (times = 0;
+	times <10;
+	times++)
+	{
+		for alpha = 'a';
+		alpha <= 'z';
+		alpha++)
+		{
+			_putchar(alpha);
+		}
+
+	}
+
+	_putchar('\n');
+
+}
