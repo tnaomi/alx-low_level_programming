c269b4587208dc6d37f1926e78d170d9c5a83f20
Tadala <naomikape@gmail.com>
2023-08-15 11:38:36 -0700

    Fixed & %

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,15 @@
 /**
  * print_last_digit - finds the last integer of n,
  * @n : the variable to be checked
- * Return : the last integer
+ * Return : the value of the last digit
  */
 
 int print_last_digit(int n)
 {
 
-	int mod = n & 10;
+	int mod;
+
+	mod = n % 10;
 	if (mod < 0)
 	{
 		return (-1 * mod);
