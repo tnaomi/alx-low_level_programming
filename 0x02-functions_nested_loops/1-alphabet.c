3c590ce678adbda4212dbc3879010fa837176b7f
Tadala <naomikape@gmail.com>
2023-08-15 05:13:12 -0700

    Added print_alphabet to_library

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,14 @@
 #include "main.h"
 
+
+/**
+ * main - print the letters of the alphabet
+ *followed by a newline
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	for (letter = 'a';
+	b
