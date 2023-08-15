100727811c03bb338d33355bf2b3af1a28629c2a
Tadala <naomikape@gmail.com>
2023-08-15 08:33:13 -0700

    Initialised Task Library

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks whether 'c' is alpha,
+ * @c : the variable to be checked
+ * Return: 1 if letter, 0 if otherwise
+ */
+
+int _ialpha(int c)
+{
+	int a = 65;
+
+	if (c >= a)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
