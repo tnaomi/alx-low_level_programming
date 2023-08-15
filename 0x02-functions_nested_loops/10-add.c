9811a0e70b2442e83c3b1aaa96f93cd09e2f97f1
Tadala <naomikape@gmail.com>
2023-08-15 13:24:23 -0700

    Used sum var

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add- adds two integers, returns result
+ * @int m, int n: numbers to be added
+ * Return: sum
+ */
+
+int add(int m, int n)
+{
+
+	sum = int m + int n;
+	return (sum);
+}
