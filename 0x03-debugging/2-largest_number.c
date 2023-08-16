09552a82d0999d6c736416783c925b8970a68bd4
Tadala <naomikape@gmail.com>
2023-08-16 06:52:43 -0700

    used nested if..else

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,37 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	if (a == b || a == c)
+	{
+		largest = a;
+		printf("%d\n", largest);
+	}
 
-    return (largest);
+	else
+	{
+		if (a > b)
+		{
+			if (b > c)
+			{
+				if (a > c)
+					{
+					    largest = a;
+					    printf("%d\n", largest);
+					}
+			}
+		}
+		else if (a < b && b > c && a < c)
+		{
+		    largest = b;
+		    printf("%d\n", largest);
+		}
+		else
+		{
+		    largest = c;
+		    printf("%d\n", largest);
+		}
+	}
+	return (largest);
 }
