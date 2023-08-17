5a036eeea70e97b35f08ad7f20774690a914ab57
Tadala <naomikape@gmail.com>
2023-08-17 06:06:47 -0700

    used nested if

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most__numbers - prints from 0 to 9,
+ * except 2 and 4, followed by a new line
+ * Return:Nothing. It's a void function
+ */
+
+void print_most_numbers(void)
+
+{
+
+int number;
+
+for (number = 48;
+number < 58;
+number++)
+{
+	if (number != 50 && number != 52)
+	{
+		_putchar(number);
+	}
+}
+_putchar ('\n');
+
+}
