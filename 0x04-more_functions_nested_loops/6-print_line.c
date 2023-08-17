8e765d63369b38e2f34e522ab23c0257f207a204
Tadala <naomikape@gmail.com>
2023-08-17 10:26:48 -0700

    Task 6 For_loop

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints character _ for n times
+ * @n : the number of times to print _
+ *  Return:Nothing. It's a void function
+ */
+
+void print_most_numbers(void)
+
+{
+
+int number;
+
+for (number = 0; number < n; number++)
+{
+_putchar('_');
+}
+_putchar('\n');
+
+}
