1220976d2d33f88b5219db479874d636aa7d9a31
Tadala <naomikape@gmail.com>
2023-08-17 05:19:14 -0700

    Edit library Task_3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a : the variable to be multiplied
+ * @b : the variable to multiply a with
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+
+{
+
+int product;
+
+product = a * b;
+
+return (product);
+
+}
