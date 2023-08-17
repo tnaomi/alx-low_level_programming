b6e7ad27c50636dd3bb246dfdf285f037de32af0
Tadala <naomikape@gmail.com>
2023-08-17 11:50:59 -0700

    Added for loop

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,16 +11,21 @@ void print_square(int size)
 {
 
 int number;
-/*
-* int number2;
-*/
+
+int number2;
+
 if (size <= 0)
 _putchar('\n');
 for (number = 0;
 number < size;
 number++)
 {
+for (number2 = 0;
+number2 < number;
+number2++)
+{
 _putchar('#');
+}
 }
 _putchar('\n');
 
