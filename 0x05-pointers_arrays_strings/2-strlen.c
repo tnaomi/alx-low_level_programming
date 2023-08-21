8baa55befc29b156a6437907006ef24c691507c2
Tadala <naomikape@gmail.com>
2023-08-21 10:03:07 -0700

    store memory

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*  _strlen - prints the length of a string
+*  @s: pointer to string location
+*  Return: string length
+*/
+
+int _strlen(char *s)
+
+{
+
+int store;
+
+store = &s;
+
+}
