4b265617bbaa19a21fdbbc9835a28b0f2019f226
Tadala <naomikape@gmail.com>
2023-08-22 08:45:01 -0700

    Used str++

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*  _puts - prints string
+*  @str: pointer to input string
+*  Return: string
+*/
+
+void _puts(char *str)
+
+{
+
+int count;
+
+for (count = 0; count <= *str; *str++)
+{
+_putchar(*str);
+count++;
+}
+
+_putchar('\n');
+
+}
