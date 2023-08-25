5de304c9225674cf1d5c579abe20bee1d2888a87
Tadala <naomikape@gmail.com>
2023-08-25 14:19:14 -0700

    String copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncpy - concatenate 2 strings up to n bytes
+ * @dest:destination string
+ * @src:source string
+ * @n : bytes up to which to copy the string
+ * Return:string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+char *str = dest;
+int up_to;
+int indx = 0;
+
+for (up_to = 0; src[up_to] != '\0' && up_to < n;)
+{
+
+dest[indx] = src[up_to];
+up_to++;
+indx++;
+
+}
+for (; up_to < n;)
+{
+dest[up_to] = '\0';
+up_to++;
+}
+
+
+return (str);
+}
