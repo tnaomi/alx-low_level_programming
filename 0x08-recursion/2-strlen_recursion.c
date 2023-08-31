83e5eff422bc54de8e80a071ae00801a543ee8aa
Tadala <naomikape@gmail.com>
2023-08-31 11:57:06 -0700

    find strlen (recursion)

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,23 @@
-#include
+#include "main.h"
+
+/**
+ * _strlen_recursion - find length of string
+ * @s: string input
+ * Return:length of string: integer
+ */
+
+
+int _strlen_recursion(char *s)
+{
+
+if (!*s)
+{
+return (0);
+}
+else
+{
+return (1 + _strlen_recursion(s + 1));
+}
+putchar('\n');
+
+}
