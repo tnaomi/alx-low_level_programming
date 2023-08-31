b83d798adf260a2b1f2b4e18990c0a8f31a014d1
Tadala <naomikape@gmail.com>
2023-08-30 20:00:24 -0700

    Power recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - find x raised to y,
+ * if y -ve, error
+ * @x: number
+ * @y: power
+ * Return: integer
+ */
+
+
+int _pow_recursion(int x, int y);
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0 || x == 1)
+{
+return (1);
+}
+else if (y == 1)
+{
+return (x);
+}
+else
+{
+
+return(x * _pow_recursion(x, y));
+
+}
+putchar('\n');
+
+}
