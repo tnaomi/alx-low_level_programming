0f660c5c5811b4914150fec1ad3d1045b789d460
Tadala <naomikape@gmail.com>
2023-09-01 07:53:44 -0700

    Removed echos

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-
-echo "Objects compiling"
-
 gcc -c -Wall -Werror -Wextra *.c
-
-echo "DONE!"
-
-echo "Creating library now."
-echo ".."
-echo "..."
 
 ar -rc liball.a *.o
 
-echo "Done..."
 ls -l
 
-echo "Indexing library"
 ranlib liball.a
-
-echo "Creation of static library. Done. Exiting..."
