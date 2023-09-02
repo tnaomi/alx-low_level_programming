8b48497ad642561412798411ee32bb9cbdf5970a
Tadala <naomikape@gmail.com>
2023-09-02 12:07:38 -0700

    Print own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints own program name
+ * @argc: argument count
+ * @argv: arguments / arg vector
+ * Return: 0
+ */
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return 0;
+}
+
