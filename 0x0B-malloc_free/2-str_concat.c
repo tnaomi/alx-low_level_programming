369140c57d333a18aa943812bb35357b483b06df
Tadala <naomikape@gmail.com>
2023-09-06 07:30:20 -0700

    string concat malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * *str_concat - concantenate strings
+ * @s1: destination
+ * @s2: source
+ * Return: NULL / Ptr
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+char* ptr;
+unsigned int len, len2, index;
+
+for (len = 0; *s1 != '\0'; s1++)
+{
+len++;
+}
+for (len2 = 0; *s2 != '\0'; s2++)
+{
+len2++;
+}
+
+ptr =  malloc (len + len2 +1);
+if (ptr == NULL)
+{
+return (NULL);
+}
+
+for (index = 0; index < len;)
+{
+*(ptr + index) = *(s1 + index);
+index++;
+}
+
+for (index = 0; index < len2;)
+{
+*(ptr + len + index) = *(s2 + index);
+index++;
+}
+*ptr = '\0';
+
+
+return (ptr);
+}
