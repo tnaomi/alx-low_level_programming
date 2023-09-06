87f011da5dae4e1d371f6ba0d409281af8228010
Tadala <naomikape@gmail.com>
2023-09-06 08:20:38 -0700

    Rewrote loops

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,8 +10,8 @@
 char *str_concat(char *s1, char *s2)
 {
 
-char* ptr = s1;
-unsigned int len, len2, index;
+char *ptr;
+unsigned int len, len2, index, index2;
 
 for (len = 0; *s1 != '\0'; s1++)
 {
@@ -22,7 +22,7 @@ for (len2 = 0; *s2 != '\0'; s2++)
 len2++;
 }
 
-ptr =  malloc (len + len2 +1);
+ptr =  malloc(len + len2 + 1);
 if (ptr == NULL)
 {
 return (NULL);
@@ -30,17 +30,19 @@ return (NULL);
 
 for (index = 0; index < len;)
 {
-*(ptr + index) = *(s1 + index);
+ptr[index] = s1[index];
 index++;
 }
 
-for (index = 0; index < len2;)
+for (index2 = 0; index2 < len2;)
 {
-*(ptr + len + index) = *(s2 + index);
+ptr[index] = s2[index2];
 index++;
+index2++;
 }
-*ptr = '\0';
+ptr[index] = '\0';
 
 
 return (ptr);
+
 }
