4d131e08e52f47e00ffe98eb2776103de727b0ce
Tadala <naomikape@gmail.com>
2023-09-08 14:40:14 -0700

    Display array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * array_range - assign memory for an array
+ * @min: number of members
+ * @max: size of array
+ * Return: pointer / NULL
+ */
+
+int *array_range(int min, int max)
+{
+
+int *array;
+unsigned int indx, memb;
+
+if (min > max)
+{
+return (NULL);
+}
+
+memb = (max - min) + 1;
+array = malloc(sizeof(int) * memb);
+
+if (array == NULL)
+{
+return (NULL);
+}
+
+*array = min;
+
+for (indx = 1; min <= max;)
+{
+array[indx] = min++;
+indx++;
+}
+array[memb] = '\0';
+return (array);
+free(array);
+}
