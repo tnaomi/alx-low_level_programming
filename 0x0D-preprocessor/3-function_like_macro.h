340bf6b027d38de26ab37d15c095fea3892710fc
Tadala <naomikape@gmail.com>
2023-09-11 08:09:38 -0700

    Added parentheses Macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _3_function_like_macro
 #define _3_function_like_macro
 
-#define ABS(x) ((x) < 0) ? -(x) : (x))
+#define ABS(x) (((x) < 0) ? -(x) : (x))
 
 #endif /* 3-FUNCTION_LIKE_MACRO.H*/
