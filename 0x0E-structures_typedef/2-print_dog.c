83f2edab0b69500f5b9c85dcb8841384ebe0ca6d
Tadala <naomikape@gmail.com>
2023-09-11 12:38:00 -0700

    print struct elements

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print a new struct based
+ * on existing struct, dog.
+ * @d : pointer to struct
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+
+	if (d != NULL)
+	{
+	if (d->name == NULL)
+	{
+	    printf("Name: (nil)\n");
+	}
+	else
+	{
+	    printf("Name: %s\n",d->name);
+	}
+
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+	{
+	    printf("Owner: (nil)\n");
+	}
+
+	else
+	{
+	    printf("Owner: %s\n",d->owner);
+	}
+	}
+	else
+	{
+	printf("\n");
+	}
+ 
+}
