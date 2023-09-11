db8fb2917b9dfd817b3a707de41d7f9a6b35c08e
Tadala <naomikape@gmail.com>
2023-09-11 11:08:17 -0700

    Define struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Class of dogs
+ *
+ * @name: Dog name
+ *
+ * @age: Dog age
+ *
+ * @owner: Dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif /* DOG.H*/
