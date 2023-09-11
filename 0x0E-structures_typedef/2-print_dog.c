519047c269a47b5046a1813cda2f63459c4bad7a
Tadala <naomikape@gmail.com>
2023-09-11 12:51:11 -0700

    Fixed betty errors

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,31 +10,31 @@
 void print_dog(struct dog *d)
 {
 
-	if (d != NULL)
-	{
-	if (d->name == NULL)
-	{
-	    printf("Name: (nil)\n");
-	}
-	else
-	{
-	    printf("Name: %s\n",d->name);
-	}
+if (d != NULL)
+{
+if (d->name == NULL)
+{
+printf("Name: (nil)\n");
+}
+else
+{
+printf("Name: %s\n", d->name);
+}
 
-	printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-	{
-	    printf("Owner: (nil)\n");
-	}
+printf("Age: %f\n", d->age);
+if (d->owner == NULL)
+{
+printf("Owner: (nil)\n");
+}
 
-	else
-	{
-	    printf("Owner: %s\n",d->owner);
-	}
-	}
-	else
-	{
-	printf("\n");
-	}
- 
+else
+{
+printf("Owner: %s\n", d->owner);
+}
+}
+else
+{
+printf("\n");
+}
+
 }
