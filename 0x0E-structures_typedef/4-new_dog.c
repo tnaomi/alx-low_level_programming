d8b0bdb6e6bdc25649b3054aa617c8fe5030937c
Tadala <naomikape@gmail.com>
2023-09-12 08:15:16 -0700

    New Struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* new_dog : create a new struct based on dog_t
+* @name: Dog name
+* @age: Dog age
+* @owner: Dog owner
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	    dog_t *pntr;
+	    unsigned int indx, indxn, indxo;
+
+	    if (name == NULL || owner == NULL)
+	    {
+	        return (NULL);
+	    }
+
+	    pntr = malloc(sizeof(dog_t));
+
+	    if (pntr == NULL)
+	    {
+	        return (NULL);
+	    }
+
+	    for(indxn = 0; name[indxn];)
+	    {
+	        indxn++;
+	    }
+	    pntr->name = malloc(indxn);
+	    if (pntr == NULL)
+	    {
+	        return(NULL);
+	    }
+
+	    for ( indx = 0; indx < indxn; indx++)
+	    {
+	        pntr->name[indx] = name[indx];
+	    }
+	    pntr->age = age;
+
+	    for (indxo = 0; owner[indxo];)
+	    {
+	        indxo++;
+	    }
+	    pntr->owner = malloc(indxo);
+	    for (indx = 0; indx < indxo; indx++)
+	    {
+	        pntr->owner[indx] = owner[indx];
+	    }
+
+	    return(pntr);
+
+}
