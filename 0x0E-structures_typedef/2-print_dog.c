3a93cb1ba100a5229d1512a3cc4a0a100930d2d3
Tadala <naomikape@gmail.com>
2023-09-12 01:52:54 -0700

    Fixed indentation errors

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,26 +12,27 @@ void print_dog(struct dog *d)
 
 if (d != NULL)
 {
-if (d->name == NULL)
-{
-printf("Name: (nil)\n");
-}
-else
-{
-printf("Name: %s\n", d->name);
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+
 }
 
-printf("Age: %f\n", d->age);
-if (d->owner == NULL)
-{
-printf("Owner: (nil)\n");
-}
-
-else
-{
-printf("Owner: %s\n", d->owner);
-}
-
-}
-
-}/* End of void*/
+} /* End of void*/
