00ca47b202bf95a79bdb9864fc2f823e79425258
Tadala <naomikape@gmail.com>
2023-09-13 12:34:10 -0700

    Search an integer_array

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - fxn that searches an array
+ * for an integer and returns the first index
+ * @array: an array of integers
+ * @size: size of the array
+ * @cmp: pointer to a function that compares
+ * the array elements.
+ * Return: -1 if size < 0 & no matches, index of 1st match.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int indx;
+
+	if (size <= 0)
+		return (-1);
+
+	for (indx = 0; indx < size; indx++)
+	{
+		cmp(array[indx]);
+		if (cmp(array[indx]) != 0)
+		{
+			return (indx);
+		}
+	return (-1);
+}
