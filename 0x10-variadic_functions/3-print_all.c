d86e3ad786417e046330fa64e79f608d838a26a8
Tadala <naomikape@gmail.com>
2023-09-14 20:05:55 -0700

    Fixed indentation errors

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,44 +11,44 @@
 
 void print_all(const char * const format, ...)
 {
-int indx;
-int status;
-char *str;
-va_list input;
+	int indx;
+	int status;
+	char *str;
+	va_list input;
 
-va_start(input, format);
-indx = 0;
-while (format != NULL && format[indx] != '\0')
-{
-switch (format[indx])
-{
-case 'c':
-printf("%c", va_arg(input, int));
-status = 0;
-break;
-case 'i':
-printf("%i", va_arg(input, int));
-status = 0;
-break;
-case 'f':
-printf("%f", va_arg(input, double));
-status = 0;
-break;
-case 's':
-str = va_arg(input, char*);
-if (str == NULL)
-str = "(nil)";
-printf("%s", str);
-status = 0;
-break;
-default:
-status = 1;
-break;
-}
-if (format[indx + 1] != '\0' && status == 0)
-printf(", ");
-indx++;
-}
-_putchar("\n");
-va_end(input);
+	va_start(input, format);
+	indx = 0;
+	while (format != NULL && format[indx] != '\0')
+	{
+		switch (format[indx])
+		{
+			case 'c':
+				printf("%c", va_arg(input, int));
+				status = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(input, int));
+				status = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(input, double));
+				status = 0;
+				break;
+			case 's':
+				str = va_arg(input, char*);
+				if (str == NULL)
+					str = "(nil)";
+					printf("%s", str);
+					status = 0;
+				break;
+			default:
+				status = 1;
+				break;
+		}
+		if (format[indx + 1] != '\0' && status == 0)
+		printf(", ");
+		indx++;
+	}
+	_putchar("\n");
+	va_end(input);
 }
