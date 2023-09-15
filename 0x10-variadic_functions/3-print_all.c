d7824e20937395adfbb03f1ba08fa4c0d02197bd
Tadala <naomikape@gmail.com>
2023-09-15 12:40:55 -0700

    Print all formats

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,54 +1,51 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
- * print_all - Print all of the input arguments
- * @format: specifies the particular input format
- * Return: Nothing. Void
+ * print_all - Prints all of the arguments when specified
+ * @format: specifies the necessary operations
+ * Return: void
  */
 
 void print_all(const char * const format, ...)
 {
-	int indx;
-	int status;
+	int i;
+	int flag;
 	char *str;
-	va_list input;
+	va_list a_list;
 
-	va_start(input, format);
-	indx = 0;
-	while (format != NULL && format[indx] != '\0')
+	va_start(a_list, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
 	{
-		switch (format[indx])
+		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(input, int));
-				status = 0;
+				printf("%c", va_arg(a_list, int));
+				flag = 0;
 				break;
 			case 'i':
-				printf("%i", va_arg(input, int));
-				status = 0;
+				printf("%i", va_arg(a_list, int));
+				flag = 0;
 				break;
 			case 'f':
-				printf("%f", va_arg(input, double));
-				status = 0;
+				printf("%f", va_arg(a_list, double));
+				flag = 0;
 				break;
 			case 's':
-				str = va_arg(input, char*);
+				str = va_arg(a_list, char*);
 				if (str == NULL)
 					str = "(nil)";
-					printf("%s", str);
-					status = 0;
+				printf("%s", str);
+				flag = 0;
 				break;
 			default:
-				status = 1;
+				flag = 1;
 				break;
 		}
-		if (format[indx + 1] != '\0' && status == 0)
+		if (format[i + 1] != '\0' && flag == 0)
 			printf(", ");
-		indx++;
+		i++;
 	}
 	printf("\n");
-	va_end(input);
+	va_end(a_list);
 }
