f04cfc8cc5588454132bffa849da5484f6720514
Tadala <naomikape@gmail.com>
2023-09-21 09:07:34 -0700

    Task Print List

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_list - print all the elements of a list
+ * @h: singly-linked list h
+ * Return: number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	unsigned int indx = 0;
+
+	for (; h != NULL; indx++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+	}
+	return (indx);
+}
