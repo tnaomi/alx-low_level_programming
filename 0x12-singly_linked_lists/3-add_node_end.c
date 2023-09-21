cd7b02d30473770b0f47e9ab4665b51900e19897
Tadala <naomikape@gmail.com>
2023-09-21 12:59:09 -0700

    Append new code

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Append a new node
+ * @head: linked list head
+ * @str: Input string
+ * Return: Number of nodes
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *end_node = malloc(sizeof(list_t));
+	list_t *last = *head;
+
+	end_node->str = strdup(str);
+	end_node->len = strlen(str);
+	end_node->next = NULL;
+
+	if (*head == NULL)
+		*head = end_node;
+	else
+	{
+		while (last->next != NULL)
+		{
+			last = last->next;
+		}
+		last->next = end_node;
+	}
+	return (end_node);
+}
