7926786407f6c957c2a722ceef7b7357b9a68bf2
Tadala <naomikape@gmail.com>
2023-09-21 10:47:17 -0700

    Add new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - add a single node at the beginning
+ * @head: singly-linked list h
+ * @str: string to update structure
+ * Return: number of nodes (elements)
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		printf("NULL");
+		return (NULL);
+	}
+	else
+	{
+		new_node->str = strdup(str);
+		new_node->next = (*head);
+		(*head) = new_node;
+	}
+
+	return (new_node);
+}
