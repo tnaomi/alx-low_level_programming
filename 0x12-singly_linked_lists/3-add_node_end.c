e18976b7e76187a41ae89d2ffb08bcabc65b1db6
Tadala <naomikape@gmail.com>
2023-09-21 19:34:43 -0700

    Added memory-fail checks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,8 +12,16 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *end_node = malloc(sizeof(list_t));
 	list_t *last = *head;
 
+	if (end_node == NULL)
+		return (NULL);
 	end_node->str = strdup(str);
 
+	if (end_node->str == NULL)
+	{
+		free(end_node);
+		return (NULL);
+	}
+	end_node->len = strlen(str);
 	end_node->next = NULL;
 
 	if (*head == NULL)
