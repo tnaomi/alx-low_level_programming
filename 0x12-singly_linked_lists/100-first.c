6fc387857c4445834b2cfe0c9fc0b88a6609ee1e
Tadala <naomikape@gmail.com>
2023-09-21 19:58:37 -0700

    Print string first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * start_first - prints a string first before prog called
+ * Return: void
+*/
+
+void start_first(void) __attribute__ ((constructor));
+
+void start_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
