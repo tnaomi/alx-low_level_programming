37b40c3006d67e92eae5d0055af1afab4c0f1546
Tadala <naomikape@gmail.com>
2023-09-26 17:51:33 -0700

    Print list length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - Print number of elements in a list
+ * @h: pointer to linked list
+ * Return: number of nodes/ 1 if error
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	unsigned int num = 0;
+
+	if (h == NULL)
+		return (1);
+
+	for (; h != NULL; num++)
+		h = h->next;
+
+	return (num);
+}
