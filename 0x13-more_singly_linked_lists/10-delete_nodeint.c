cd6b5bcbea62eb55d3887142608a70493e89b0bc
Tadala Kapengule <57710314+tnaomi@users.noreply.github.com>
2023-09-27 18:42:51 +0200

    Delete specific node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,18 +9,19 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = *head, *del;
+	listint_t *temp, *del;
 	unsigned int i = 0;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
-
+	
+	temp = *head;
+	
 	if (index == 0)
 	{
-		/*pop_listint(&(*head));*/
-		*head = *head->next;
-		temp->next = NULL;
+		temp = temp->next;
 		free(temp);
+		temp = temp->next;
 		return (1);
 	}
 
