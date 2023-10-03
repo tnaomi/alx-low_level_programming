79d2ef2a595eac7e2d56df827c62d85fdc2015ca
Tadala <naomikape@gmail.com>
2023-10-02 19:35:40 -0700

    Print file contents

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - Output to stdout
+ * @filename: pointer to filename
+ * @letters: buffer size
+ * Return: 0 if ERROR
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int status;
+	ssize_t read_f, write_f;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	status = open(filename, O_RDONLY);
+
+	if (status == -1)
+		return (0);
+
+	buffer = malloc(letters);
+
+	if (buffer == NULL)
+		return (0);
+	read_f = _rd(status, buffer, letters);
+
+	write_f = _wr(11, buffer, read_f);
+
+	close(status);
+
+	free(buffer);
+
+	return (write_f);
+}
