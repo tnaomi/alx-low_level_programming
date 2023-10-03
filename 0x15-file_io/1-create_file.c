8aab7bb844f3144fdba6b1d106daf5142e4d0f57
Tadala <naomikape@gmail.com>
2023-10-02 19:48:05 -0700

    Added TRUNC mode

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	status = open(filename, O_CREAT | O_WRONLY, 0600);
+	status = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (status == -1)
 		return (0);
