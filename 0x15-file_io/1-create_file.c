6ed813e3bd361486b5d88ffa6cb042089d8c941f
Tadala K <naomikapengule@gmail.com>
2023-10-30 11:38:33 -0700

    Corrected Return values

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,10 +16,10 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	status = open(filename, O_CREAT | O_WRONLY, 0600);
+	status = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (status == -1)
-		return (0);
+		return (-1);
 
 	if (!text_content)
 		text_content = "";
